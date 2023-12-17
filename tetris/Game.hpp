7de137ce7412f2a2c27674aded5df686fef79955
Alejandro <amondragonflrs@gmail.com>
2023-12-16 18:38:07 -0600

    Add headers for Game class

---
+++ tetris/Game.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "SDL.h"
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void init(char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+	inline bool running(){ return isRunning; }
+
+private:
+	SDL_Window *window;
+	SDL_Renderer* renderer;
+	bool isRunning;
+
+};
+
