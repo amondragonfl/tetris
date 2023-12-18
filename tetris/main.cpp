a64f701df5d6acc52657e352fa1f09a062850ab0
Alejandro <amondragonflrs@gmail.com>
2023-12-17 22:20:53 -0600

    Add basic gameloop structure

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -1,6 +1,21 @@
 #include "SDL.h"
+#include "Game.hpp"
+
+Game *game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	game = new Game();
+
+	game->init("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 300, 400, false);
+
+	while (game->running())
+	{
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	game->clean();
+
 	return 0;
 }
