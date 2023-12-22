1a317af11a96cce3b898597bfd22e22d02f2e636
Alejandro <amondragonflrs@gmail.com>
2023-12-22 12:20:29 -0600

    Set a target framerate

--- tetris/main.cpp
+++ tetris/main.cpp
@@ -5,14 +5,25 @@ Game *game = nullptr;
 
 int main(int argc, char* argv[])
 {
+	const int FPS = 60;
+	const int frameDelay = 1000/FPS; // target time per frame  
+
+	Uint32 frameStart;
+	int frameTime; 
+
 	game = new Game();
 	game->init("Tetris", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 250, 425, false);
 
 	while (game->running())
 	{
+		frameStart = SDL_GetTicks();
 		game->handleEvents();
 		game->update();
 		game->render();
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) { SDL_Delay(frameDelay - frameTime); } 
+
 	}
 	game->clean();
 
