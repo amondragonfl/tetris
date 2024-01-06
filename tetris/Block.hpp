16a22f6f4af7708e076597b3e00bae5d818eb708
Alejandro <amondragonflrs@gmail.com>
2024-01-06 00:18:51 -0600

    Add setter and getter methods

--- tetris/Block.hpp
+++ tetris/Block.hpp
@@ -8,6 +8,12 @@ public:
 	~Block();
 	void update();
 	void render();
+	inline void setX(int x) { xpos = x; }
+	inline void setY(int y) { ypos = y; }
+	inline void setRow(int r) { row = r; }
+	inline void setCol(int c) { col = c;  }
+	inline int getRow() const { return row; }
+	inline int getCol() const { return col; }
 	
 private:
 	SDL_Renderer* renderer; 
